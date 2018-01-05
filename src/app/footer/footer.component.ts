c848a09be3cc53015004fec95187f469396f5895
jgonzalez <jgonzalez@opencanarias.es>
2018-01-05 14:19:57 +0000

    Añadir un nuevo componente
    Nuevo componente de ejemplo

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../redux/app.state';
+import { SetFilterAction } from '../../redux/filter/filter.actions';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  constructor(
+    private store: Store<AppState>
+  ) {}
+
+  ngOnInit() {
+
+  }
+
+  doFilter(filter: string) {
+    const action = new SetFilterAction(filter);
+    this.store.dispatch(action);
+  }
+}
