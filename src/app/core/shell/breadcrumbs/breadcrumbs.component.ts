7dee6cd9abdb01ef42981108b468803e712f0650
Juan Antonio González Cano <jantoniogc@gmail.com>
2018-02-01 09:47:01 +0000

    Añadimos meta tags y usamos filter, map y observables en la aplicación real para cambiar el breadcrumbs

--- src/app/core/shell/breadcrumbs/breadcrumbs.component.ts
+++ src/app/core/shell/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import { Title } from '@angular/platform-browser';
+import { Meta, MetaDefinition} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,8 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  public pageLabel: string = '';
 
+  constructor(
+    private router: Router,
+    private title: Title,
+    private meta: Meta
+  ) {
+      this.getDataRoute()
+        .subscribe( data => {
+          this.pageLabel = data.titulo;
+          this.title.setTitle(this.pageLabel);
+          let metaTag: MetaDefinition = {
+            name : 'Description',
+            content: this.pageLabel
+          }
+          this.meta.updateTag(metaTag);
+        })
+  }
+
+  getDataRoute() {
+    return this.router.events
+      .filter(evento => evento instanceof ActivationEnd)
+      .filter((evento: ActivationEnd) => evento.snapshot.firstChild === null)
+      .map((evento: ActivationEnd) => evento.snapshot.data);
+  }
   ngOnInit() {
   }
 
--- src/app/core/shell/shell.routes.ts
+++ src/app/core/shell/shell.routes.ts
@@ -4,30 +4,32 @@ import { RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'dashboard',
+    data: { titulo: 'Dashboard' },
     loadChildren: '../dashboard/dashboard.module#DashboardModule'
   },
   {
     path: 'progress',
+    data: { titulo: 'Progress' },
     loadChildren: '../progress/progress.module#ProgressModule'
   },
   {
     path: 'graficas1',
-    loadChildren: '../../graficas1/graficas1.module#Graficas1Module'
-  },
-  {
-    path: 'graficas1',
+    data: { titulo: 'Graficas'},
     loadChildren: '../../graficas1/graficas1.module#Graficas1Module'
   },
   {
     path: 'promesas',
+    data: { titulo: 'Progresas' },
     loadChildren: '../../pages/promesas/promesas.module#PromesasModule'
   },
   {
     path: 'rxjs',
+    data: { titulo: 'Rxjs' },
     loadChildren: '../../pages/rxjs/rxjs.module#RxjsModule'
   },
   {
     path: 'account-settings',
+    data: { titulo: 'Account Settings' },
     loadChildren: '../../pages/account-settings/account-settings.module#AccountSettingsModule'
   },
    {
